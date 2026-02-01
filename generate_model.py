da6a8a10ec9ba7079dcaf00b8a7e2e25f776ee94
Gaurav Karakoti <karakotigaurav12@gmail.com>
2026-02-01 13:02:03 +0000

    LSTM Model

---
+++ generate_model.py
@@ -0,0 +1,20 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+import tensorflow as tf
+
+X_train = np.random.rand(100, 1, 1)
+y_train = np.random.rand(100, 1)
+
+model = Sequential([
+    LSTM(16, activation='relu', input_shape=(1, 1)),
+    Dense(1)
+])
+
+model.compile(optimizer='adam', loss='mse')
+
+print("Generating model weights...")
+model.fit(X_train, y_train, epochs=1, verbose=0)
+
+model.save("traffic_lstm.h5")
+print("✅ Success! 'traffic_lstm.h5' has been generated.")
